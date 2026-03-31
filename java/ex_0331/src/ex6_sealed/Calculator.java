3e2664e2f38575a6474a67b2039bb560b48ef4c1
swj <vhehf369@gmail.com>
2026-03-31 18:35:23 +0900

    web-swj0331

---
+++ java/ex_0331/src/ex6_sealed/Calculator.java
@@ -0,0 +1,11 @@
+package ex6_sealed;
+
+public class Calculator {
+
+	public int getResult(int n1, int n2) {
+		int n1;
+		int n2;
+		
+		return -1;
+	}
+}
---
+++ java/ex_0331/src/test/BallPen.java
@@ -0,0 +1,12 @@
+package test;
+
+public class BallPen {
+	private String color;
+	
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getColor() {
+		return color;
+	}
+}
